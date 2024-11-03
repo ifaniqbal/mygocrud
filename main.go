a4416ffaed9d057848dd60bd8a2e6d21f95fbe24
Ifan Iqbal <ifaniqbal.com@gmail.com>
2024-11-03 11:33:11 +0700

    feat: finalize create and read endpoints

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"mygocrud/model"
 	"mygocrud/service"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -26,10 +29,68 @@ func main() {
 	r.POST("/items", service.CreateItemHandler)
 	r.GET("/items", service.ReadItemsHandler)
 
+	r.POST("/users", CreateUserHandler)
+	r.GET("/users", ReadUsersHandler)
+
 	// Start server
 	err = r.Run(":8080")
 	if err != nil {
 		log.Fatalln(err)
 		return
 	}
+}
+
+func ReadUsersHandler(c *gin.Context) {
+	var users []model.User
+
+	query := `select * from users`
+	filter := c.Query("filter")
+	if filter != "" {
+		query = fmt.Sprintf(
+			"%s %s",
+			query,
+			"where username = ?",
+		)
+	}
+
+	// select * from users where username = ?
+
+	err := db.Raw(query, filter).Scan(&users).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.BaseResponse{
+			Message: fmt.Sprintf("failed to save user: %s", err.Error()),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, model.BaseResponse{
+		Success: true,
+		Message: "Success",
+		Data:    users,
+	})
+}
+
+func CreateUserHandler(c *gin.Context) {
+	var user model.User
+	err := c.ShouldBind(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, model.BaseResponse{
+			Message: fmt.Sprintf("failed to bind request: %s", err.Error()),
+		})
+		return
+	}
+
+	err = db.Create(&user).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.BaseResponse{
+			Message: fmt.Sprintf("failed to save user: %s", err.Error()),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, model.BaseResponse{
+		Success: true,
+		Message: "Success",
+		Data:    user,
+	})
 }
--- model/user.go
+++ model/user.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type User struct {
-	ID           int
-	Username     string
-	Email        string
-	PasswordHash string
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"passwordHash"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
 }

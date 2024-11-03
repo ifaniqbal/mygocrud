a77099ed3d47c9c3a71fac87a6eb9f64757de566
Ifan Iqbal <ifaniqbal.com@gmail.com>
2024-11-03 11:06:49 +0700

    feat: add database connection

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type BaseRequest struct {
@@ -28,7 +30,17 @@ type Item struct {
 	PostingDate string `json:"posting_date"`
 }
 
+var db *gorm.DB
+
 func main() {
+	// Database connection
+	dsn := "root:@tcp(127.0.0.1:3306)/ecommerce?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
 	r := gin.Default()
 
 	// Routes
@@ -36,7 +48,7 @@ func main() {
 	r.GET("/items", ReadItemsHandler)
 
 	// Start server
-	err := r.Run(":8080")
+	err = r.Run(":8080")
 	if err != nil {
 		log.Fatalln(err)
 		return

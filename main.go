dce2cd782e7e86e9f4920040a144dcd3b2cdee0a
Ifan Iqbal <ifaniqbal.com@gmail.com>
2024-11-16 09:55:26 +0700

    feat: replicate upload file endpoint using builtin Go net/http

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"mygocrud/repository"
 	"mygocrud/service"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -37,7 +39,31 @@ func main() {
 	r.POST("/upload-product-image", service.UploadProductImageHandler)
 
 	// Start server
-	err = r.Run(":8080")
+	//err = r.Run(":8080")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//	return
+	//}
+
+	http.HandleFunc("/upload-product-image", func(w http.ResponseWriter, r *http.Request) {
+		formFile, file, err := r.FormFile("image")
+		if err != nil {
+			_, _ = fmt.Fprintln(w, fmt.Sprintf("failed to get uploaded file: %s", err.Error()))
+			return
+		}
+		defer formFile.Close()
+
+		maxFileSize := 100 << 10 // 10kb
+		if file.Size > int64(maxFileSize) {
+			_, _ = fmt.Fprintln(w, "file size exceed maximum allowed")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = fmt.Fprintln(w, `{"message": "Success"}`)
+	})
+
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalln(err)
 		return
